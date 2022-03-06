d3e878f8fdc10a27f5819b4cb717038dd4a512ea
sweetcapucino <thuylinh271055@gmail.com>
2022-03-06 23:14:18 +0700

    M: about us

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
 
-import roller from "./assets/roller.png";
+import roller from "./assets/images/roller.png";
+import bread1 from "./assets/images/bread-1.svg";
 import about1 from "./assets/images/about-1.png";
+import about2 from "./assets/images/about-2.png";
 
 // config tailwindcss
 import "./main.css";
@@ -38,7 +40,29 @@ function App() {
                     <div className="font-title text-[200px] text-white-rgba text-left pl-28 absolute top-0">
                         About us
                     </div>
-                    {/* <img className="pt-16" src={about1} alt="image" /> */}
+                    <div>
+                        <img className="absolute right-64 top-40" src={bread1} alt="bread-1" />
+                    </div>
+                    <div className="w-[66.25rem] mx-auto pt-36">
+                        <img src={about1} alt="about-1" />
+                        <div className="pt-7 flex">
+                            <img src={about2} alt="about-2" />
+                            <div className="pl-12 pt-16 text-left">
+                                <div className="text-lg">
+                                    SMOKEY BOURBON TOFFEE CHOCOLATE OAT COOKIES
+                                </div>
+                                <div className="pt-6">
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Porttitor vulputate cursus
+                                    id erat purus quam. Semper egestas ipsum
+                                    pellentesque sollicitudin. Nunc pellentesque
+                                    ut posuere condimentum nulla. Imperdiet
+                                    mauris accumsan elit varius posuere lectus
+                                    in habitant sed.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import banner1 from "../assets/banner-1.png";
+import banner1 from "../assets/images/banner-1.png";
 
 export default function Carousel() {
     return (
         <div className="w-screen h-screen absolute top-0 bg-no-repeat bg-cover">
-            <img  src={banner1} alt="" />
+            <img src={banner1} alt="banner" />
         </div>
     );
 }
