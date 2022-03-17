ae64e49ecf86f3d2d6d8c0f7b0947e9bd0d43062
sweetcapucino <thuylinh271055@gmail.com>
2022-03-17 17:49:38 +0700

    U: searchbar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import search from "../assets/icons/search.svg";
+
+export default function SearchBar() {
+    return (
+        <div className="flex bg-dark-bg border border-border rounded py-3.5 px-3.5 w-96">
+            <img src={search} alt="icon search" />
+            <input
+                className="bg-dark-bg pl-2"
+                type="text"
+                id="fname"
+                name="firstname"
+                placeholder="Type something..."
+            />
+        </div>
+    );
+}
