b4a02689f7fbf75934137d4afac757ef7794c4b3
sweetcapucino <thuylinh271055@gmail.com>
2022-03-17 20:19:59 +0700

    U: product card horizontal component

---
+++ src/components/Dashboard/ProductCardHorizontal.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import cake from "../../assets/images/blog/blog-2.png";
+import cartIcon from "../../assets/icons/cart.svg";
+
+export default function ProductCardHorizontal() {
+    return (
+        <div className="bg-dark-bg py-4 px-4 rounded-lg flex space-x-4">
+            <img
+                className="h-[100px] w-[100px] object-cover"
+                src={cake}
+                alt="cake"
+            />
+            <div className="text-sm h-full">
+                <div className="font-medium text-primary">
+                    Choco Protein Brownie
+                </div>
+                <div className="pt-1">60.000 VND</div>
+                <button className="border border-primary bg-primary pl-2 pr-2.5 py-1 mt-1.5 h-max rounded-md flex items-center">
+                    <img className="mr-1" src={cartIcon} alt="cart icon" />
+                    Add
+                </button>
+            </div>
+        </div>
+    );
+}
