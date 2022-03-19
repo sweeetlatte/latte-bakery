29fe83d7b634781d8e7e25e627b35c82006262d2
sweetcapucino <thuylinh271055@gmail.com>
2022-03-19 16:34:14 +0700

    M: search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,7 +7,7 @@ export default function SearchBar() {
         <div className="flex bg-dark-bg border border-border rounded py-3.5 px-3.5 w-96">
             <img src={search} alt="icon search" />
             <input
-                className="bg-dark-bg pl-2"
+                className="bg-dark-bg pl-2 w-full focus:outline-none"
                 type="text"
                 id="fname"
                 name="firstname"
