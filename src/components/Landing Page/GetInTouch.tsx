1666dd58942feceb14758294c92b35d3f6f0ca06
sweetcapucino <thuylinh271055@gmail.com>
2022-04-13 14:06:12 +0700

    latte/style (getintouch): cursor-pointer for icons

--- src/components/Landing Page/GetInTouch.tsx
+++ src/components/Landing Page/GetInTouch.tsx
@@ -54,12 +54,29 @@ export default function GetInTouch() {
                         <div>+234 081-1236-4568</div>
                         <div>hello@info.com.ng</div>
                         <div className="flex space-x-4">
-                            <img src={youtube} alt="youtube" />
-                            <img src={instagram} alt="instagram" />
-                            <img src={facebook} alt="facebook" />
-                            <img src={twitter} alt="twitter" />
+                            <img
+                                className="cursor-pointer"
+                                src={youtube}
+                                alt="youtube"
+                            />
+                            <img
+                                className="cursor-pointer"
+                                src={instagram}
+                                alt="instagram"
+                            />
+                            <img
+                                className="cursor-pointer"
+                                src={facebook}
+                                alt="facebook"
+                            />
+                            <img
+                                className="cursor-pointer"
+                                src={twitter}
+                                alt="twitter"
+                            />
                         </div>
                         <iframe
+                            title="map"
                             className="w-full rounded"
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15672.26066752016!2d106.7824544!3d10.88264815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527587e9ad5bf%3A0xafa66f9c8be3c91!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2jhu4cgVGjDtG5nIHRpbiAtIMSQSFFHIFRQLkhDTQ!5e0!3m2!1svi!2s!4v1646993995829!5m2!1svi!2s"
                             loading="lazy"
