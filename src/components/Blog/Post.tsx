8da3192e430202c10697c025e06a605f6b4a1a85
sweetcapucino <thuylinh271055@gmail.com>
2022-04-17 21:24:23 +0700

    latte/feat (post component): add link

--- src/components/Blog/Post.tsx
+++ src/components/Blog/Post.tsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import post1 from "../../assets/images/blog/blog-11.png";
 
 export default function Post() {
     return (
         <div className="pb-9 flex items-center">
-            <img
-                className="w-[133px] h-[123px] object-cover"
-                src={post1}
-                alt="post 1"
-            />
-            <div className="pl-3 text-sm">
-                <div className="font-light pb-3.5 text-overflow h-[44px]">
+            <Link to="/blog/detail" className="w-[133px]">
+                <img
+                    className="w-full h-[123px] object-cover"
+                    src={post1}
+                    alt="post 1"
+                />
+            </Link>
+            <div className="pl-3 text-sm basis-9/12">
+                <Link
+                    to="/blog/detail"
+                    className="font-light pb-3.5 text-overflow h-[44px]"
+                >
                     SMOKEY BOURBON TOFFEE CHOCOLATE OAT COOKIES
-                </div>
-                <div>25 minutes ago</div>
+                </Link>
+                <div className="font-extralight">25 minutes ago</div>
             </div>
         </div>
     );
