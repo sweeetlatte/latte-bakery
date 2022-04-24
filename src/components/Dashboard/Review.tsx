3a2be50bd8b12f909b1634e98cbe3feabc73a7aa
sweetcapucino <thuylinh271055@gmail.com>
2022-04-24 17:29:08 +0700

    latte/feat (detail product): like + dislike button

--- src/components/Dashboard/Review.tsx
+++ src/components/Dashboard/Review.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Icons from "../Icons";
 
 import ava from "../../assets/images/blog/detail/detail-1.png";
 
 export default function Review() {
+    const [isHover, setIsHover] = useState<null | boolean>(null);
+    const [isClick, setIsClick] = useState<null | boolean>(null);
+
     return (
         <div className="flex space-x-4">
             <img
@@ -19,8 +22,34 @@ export default function Review() {
                         <div>🤎🤎🤎🤎🤎</div>
                     </div>
                     <div className="flex space-x-5">
-                        <Icons.ThumbsDown />
-                        <Icons.ThumbsUp />
+                        <div
+                            onMouseEnter={() => setIsHover(true)}
+                            onMouseLeave={() => setIsHover(null)}
+                            onClick={() => {
+                                setIsClick(true);
+                            }}
+                        >
+                            <Icons.ThumbsDown
+                                stroke={
+                                    isHover === true ? "#F3A446" : "#FFFFFF"
+                                }
+                                fill={isClick === true ? "#F3A446" : "none"}
+                            />
+                        </div>
+                        <div
+                            onMouseEnter={() => setIsHover(false)}
+                            onMouseLeave={() => setIsHover(null)}
+                            onClick={() => {
+                                setIsClick(false);
+                            }}
+                        >
+                            <Icons.ThumbsUp
+                                stroke={
+                                    isHover === false ? "#F3A446" : "#FFFFFF"
+                                }
+                                fill={isClick === false ? "#F3A446" : "none"}
+                            />
+                        </div>
                     </div>
                 </div>
                 <div className="bg-light p-3 rounded-lg">
