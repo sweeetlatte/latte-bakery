df9b1c56417d450a716e9085e1022e3cef1fb4c9
sweetcapucino <thuylinh271055@gmail.com>
2022-04-24 17:29:39 +0700

    latte/chore

--- src/components/Landing Page/ScrollToTop/index.tsx
+++ src/components/Landing Page/ScrollToTop/index.tsx
@@ -2,43 +2,50 @@ import React, { useEffect, useState } from "react";
 import Icons from "../../Icons";
 
 export default function ScrollToTop() {
-  const [isVisible, setIsVisible] = useState(false);
-  const [isHover, setIsHover] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+    const [isHover, setIsHover] = useState(false);
 
-  // Top: 0 takes us all the way back to the top of the page
-  // Behavior: smooth keeps it smooth!
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  };
-
-  useEffect(() => {
-    // Button is displayed after scrolling for 500 pixels
-    console.log("abc");
-    
-    const toggleVisibility = () => {
-      if (window.pageYOffset > 500) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
+    // Top: 0 takes us all the way back to the top of the page
+    // Behavior: smooth keeps it smooth!
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
     };
 
-    window.addEventListener("scroll", toggleVisibility);
+    useEffect(() => {
+        // Button is displayed after scrolling for 500 pixels
+        console.log("abc");
 
-    return () => window.removeEventListener("scroll", toggleVisibility);
-  }, []);
+        const toggleVisibility = () => {
+            if (window.pageYOffset > 500) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
 
-//scroll-to-top classes: fixed, bottom:0, right:0
-  return (
-    <div className="fixed right-10 bottom-10 z-20" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
-      {isVisible && (
-        <div className="flex justify-center items-center border-primary border w-12 h-12 rounded-lg z-20 hover:bg-primary" onClick={scrollToTop}>
-          <Icons.ArrowUp stroke={ isHover ? "#FFFFFF" : "#F3A446"} />
+        window.addEventListener("scroll", toggleVisibility);
+
+        return () => window.removeEventListener("scroll", toggleVisibility);
+    }, []);
+
+    //scroll-to-top classes: fixed, bottom:0, right:0
+    return (
+        <div
+            className="fixed right-10 bottom-10 z-20"
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+        >
+            {isVisible && (
+                <div
+                    className="flex justify-center items-center border-primary border w-12 h-12 rounded-lg z-20 hover:bg-primary"
+                    onClick={scrollToTop}
+                >
+                    <Icons.ArrowUp stroke={isHover ? "#FFFFFF" : "#F3A446"} />
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
