f72a18a5e70ba2b3a403dc75553d0f45732d296f
sweetcapucino <thuylinh271055@gmail.com>
2022-09-02 13:04:16 +0700

    style (forgotPassword): default xl lg md sm

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,32 +1,22 @@
-import React from "react";
 import { Link } from "react-router-dom";
-
-import login from "../assets/images/login.png";
 
 export default function ForgotPassword() {
     return (
-        <div className="text-white bg-dark-bg">
-            <img
-                className="object-cover absolute h-screen"
-                src={login}
-                alt="login"
-            />
-            <div className="relative h-screen px-48 py-20 rounded-l-3xl bg-light ml-[24.5rem] flex flex-col justify-between">
-                <div>
+        <div className="text-white bg-dark-bg h-screen login xl:text-sm sm:text-xs">
+            <div className="flex flex-col h-full px-[11%] py-[6%] ml-[24%] sm:ml-0 rounded-l-3xl sm:rounded-none bg-light">
+                <div className="basis-1/2">
                     <Link
                         to="/"
-                        className="text-primary text-4xl font-semibold font-brand"
+                        className="text-primary text-4xl lg:text-3xl md:text-2xl font-semibold font-brand"
                     >
                         Bakery
                     </Link>
-                    <div className="py-14 flex items-baseline">
-                        <p className="text-2xl font-semibold text-primary">
-                            Forgot your password?&nbsp;
-                        </p>
-                        <p>Don't worry! We got your back!</p>
-                    </div>
+                    <p className="text-2xl font-semibold text-primary pt-12 pb-3">
+                        Forgot your password?&nbsp;
+                    </p>
+                    <p>Don't worry! We got your back!</p>
                 </div>
-                <form className="mb-40 flex flex-col items-center">
+                <form className="basis-1/2 flex flex-col items-center">
                     <label htmlFor="email" className="w-full">
                         <span className="block">Email</span>
                         <input
@@ -37,13 +27,13 @@ export default function ForgotPassword() {
                             name="email"
                         />
                     </label>
-                    <div className="space-x-10">
+                    <div className="space-x-10 sm:space-x-0 sm:flex sm:flex-col-reverse sm:w-full">
                         <Link to="/login">
-                            <button className="border border-primary text-primary px-5 py-2.5 rounded-md mt-10">
+                            <button className="border border-primary text-primary px-5 py-2.5 rounded-md mt-10 sm:mt-6 sm:w-full">
                                 Back to log in
                             </button>
                         </Link>
-                        <button className="border border-primary bg-primary px-5 py-2.5 rounded-md mt-10">
+                        <button className="border border-primary bg-primary px-5 py-2.5 rounded-md mt-10 sm:mt-14">
                             Send request
                         </button>
                     </div>
