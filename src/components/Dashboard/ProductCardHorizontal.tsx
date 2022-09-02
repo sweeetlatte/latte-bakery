636b6bff2879671e690aa99c2d289c4d170f37c0
sweetcapucino <thuylinh271055@gmail.com>
2022-09-02 22:23:34 +0700

    style(productCardHorizontal): default sm

--- src/components/Dashboard/ProductCardHorizontal.tsx
+++ src/components/Dashboard/ProductCardHorizontal.tsx
@@ -29,24 +29,24 @@ export default function ProductCardHorizontal({ openModalAddProduct }: Props) {
             {randomNumber(productData, 3).map((productItem, index) => (
                 <div
                     key={index}
-                    className="bg-dark-bg py-4 px-4 rounded-lg flex xl:flex-col space-x-4 xl:space-x-0 justify-start w-full xl:w-[180px] lg:w-[150px] xl:h-[245px]"
+                    className="bg-dark-bg p-4 sm:p-5 rounded-lg flex xl:flex-col space-x-4 xl:space-x-0 justify-start w-full xl:w-[180px] lg:w-[150px] sm:w-[126px] xl:h-[245px] sm:h-[251px]"
                 >
                     <Link
-                        className="h-[100px] w-[100px] xl:h-[117px] xl:w-full"
+                        className="h-[100px] w-[100px] xl:h-[117px] sm:h-[86px] xl:w-full"
                         to="/dashboard/detail"
                         state={{ detail: productItem }}
                     >
                         <img
-                            // className="h-[100px] w-[100px] object-cover"
                             className="h-full w-full object-cover"
                             src={productItem.image}
                             alt={productItem.name}
                         />
                     </Link>
-                    <div className="text-sm h-full overflow-hidden xl:flex xl:h-fit xl:items-end">
+                    <div className="text-sm h-full overflow-hidden xl:flex xl:h-fit xl:items-end sm:flex-col sm:items-start">
                         <section className="xl:basis-1/2">
                             <Link
                                 to="/dashboard/detail"
+                                title={productItem.name}
                                 state={{ detail: productItem }}
                                 className="font-medium text-primary text-overflow w-[145px] xl:w-full xl:pt-4"
                                 style={{ WebkitLineClamp: 1 }}
@@ -60,13 +60,13 @@ export default function ProductCardHorizontal({ openModalAddProduct }: Props) {
                                 }).format(productItem.price)}
                             </div>
                         </section>
-                        <section className="xl:basis-1/2 lg:flex lg:justify-end">
+                        <section className="xl:basis-1/2 lg:flex lg:justify-end sm:w-full">
                             <button
-                                className="border border-primary bg-primary pl-2 pr-2.5 py-1 mt-5 h-max rounded-md flex items-center lg:text-[0px]"
+                                className="border border-primary bg-primary pl-2 pr-2.5 py-1 mt-5 h-max rounded-md flex items-center lg:text-[0px] sm:text-xs sm:w-full"
                                 onClick={openModalAddProduct}
                             >
                                 <img
-                                    className="lg:mr-0 xl:mr-1"
+                                    className="lg:mr-0 xl:mr-1 sm:mr-3"
                                     src={cartIcon}
                                     alt="cart icon"
                                 />
