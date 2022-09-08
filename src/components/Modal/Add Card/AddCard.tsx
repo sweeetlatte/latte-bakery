7082e83e648f01ba5b3b83ab23b7a1dbbc8b1025
sweetcapucino <thuylinh271055@gmail.com>
2022-09-08 11:13:32 +0700

    style(addCard): mask modal

--- src/components/Modal/Add Card/AddCard.tsx
+++ src/components/Modal/Add Card/AddCard.tsx
@@ -19,90 +19,100 @@ export default function AddCard({ open, closeModalAddProduct }: Props) {
     };
 
     // console.log(location);
+    console.log("modal", open);
 
     return (
         <>
             <div
-                className={open ? "__mask-modal" : "hidden"}
-                onClick={closeModalAddProduct}
-            />
-            <div
                 className={
-                    open ? "flex flex-col __bg-add-product-modal" : "hidden"
+                    open
+                        ? "__mask-modal z-20 flex justify-center items-center"
+                        : "hidden"
                 }
             >
-                <div className="flex flex-row w-full">
-                    <div className="basis-5/12">
-                        <img
-                            className="w-[17.5rem] h-[17.5rem] object-cover rounded-lg"
-                            src={cake}
-                            alt="cake"
-                        />
-                    </div>
-                    <div className="basis-7/12 flex flex-col">
-                        <div className="flex w-full relative ">
-                            <div className="text-primary font-medium">
-                                Meringue Tart
-                            </div>
+                <div
+                    className={
+                        open ? "__mask-modal __mask-modal-dark" : "hidden"
+                    }
+                    style={
+                        { "--opacity": 0.6, "--z": 20 } as React.CSSProperties
+                    }
+                    onClick={closeModalAddProduct}
+                />
+                <div className="z-30 flex flex-col __bg-add-product-modal">
+                    <div className="flex space-x-5">
+                        <div className="basis-5/12">
                             <img
-                                onClick={closeModalAddProduct}
-                                className="absolute right-[-14px] top-[-14px] cursor-pointer"
-                                src={x}
-                                alt="x"
+                                className="w-[17.5rem] h-[17.5rem] object-cover rounded-lg"
+                                src={cake}
+                                alt="cake"
                             />
                         </div>
-                        <p className="text-xl">80.000 VND</p>
-                        <p className="pt-4 text-sm">Size</p>
-                        <div className="text-sm text-primary pt-3 flex space-x-3">
-                            <button className="__yellow-outline-option py-1 px-4 w-max">
-                                Size S
-                            </button>
-                            <button className="__yellow-outline-option py-1 px-4 w-max">
-                                Size M
-                            </button>
-                            <button className="__yellow-outline-option py-1 px-4 w-max">
-                                Size L
-                            </button>
-                        </div>
-                        <p className="pt-4 text-sm">Filling</p>
-                        <div className="text-sm text-primary pt-3 flex space-x-3">
-                            <button className="__filling bg-dark-bg h-[103px]">
-                                Butter
+                        <div className="basis-7/12 flex flex-col">
+                            <div className="flex w-full relative ">
+                                <div className="text-primary font-medium">
+                                    Meringue Tart
+                                </div>
                                 <img
-                                    className="h-[60px] w-full object-cover pt-1"
-                                    src={creme}
-                                    alt="creme"
+                                    onClick={closeModalAddProduct}
+                                    className="absolute right-[-14px] top-[-14px] cursor-pointer"
+                                    src={x}
+                                    alt="x"
                                 />
-                            </button>
-                            <button className="__filling bg-dark-bg">
-                                Chocolate
-                                <img
-                                    className="h-[60px] w-full object-cover pt-1"
-                                    src={creme}
-                                    alt="creme"
-                                />
-                            </button>
-                            <button className="__filling bg-dark-bg">
-                                Crème
-                                <img
-                                    className="h-[60px] w-full object-cover pt-1"
-                                    src={creme}
-                                    alt="creme"
-                                />
-                            </button>
+                            </div>
+                            <p className="text-xl">80.000 VND</p>
+                            <p className="pt-4 text-sm">Size</p>
+                            <div className="text-sm text-primary pt-3 flex space-x-3">
+                                <button className="__yellow-outline-option py-1 px-4 w-max">
+                                    Size S
+                                </button>
+                                <button className="__yellow-outline-option py-1 px-4 w-max">
+                                    Size M
+                                </button>
+                                <button className="__yellow-outline-option py-1 px-4 w-max">
+                                    Size L
+                                </button>
+                            </div>
+                            <p className="pt-4 text-sm">Filling</p>
+                            <div className="text-sm text-primary pt-3 flex space-x-3">
+                                <button className="__filling bg-dark-bg h-[103px]">
+                                    Butter
+                                    <img
+                                        className="h-[60px] w-full object-cover pt-1"
+                                        src={creme}
+                                        alt="creme"
+                                    />
+                                </button>
+                                <button className="__filling bg-dark-bg">
+                                    Chocolate
+                                    <img
+                                        className="h-[60px] w-full object-cover pt-1"
+                                        src={creme}
+                                        alt="creme"
+                                    />
+                                </button>
+                                <button className="__filling bg-dark-bg">
+                                    Crème
+                                    <img
+                                        className="h-[60px] w-full object-cover pt-1"
+                                        src={creme}
+                                        alt="creme"
+                                    />
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="flex flex-row justify-end pt-9">
-                    <button
-                        onClick={closeModalAddProduct}
-                        className="outlined-button ml-6 w-fit"
-                    >
-                        Cancel
-                    </button>
-                    <button className="filled-button ml-6 w-fit">
-                        Add to cart
-                    </button>
+                    <div className="flex flex-row justify-end pt-9">
+                        <button
+                            onClick={closeModalAddProduct}
+                            className="outlined-button ml-6 w-fit"
+                        >
+                            Cancel
+                        </button>
+                        <button className="filled-button ml-6 w-fit">
+                            Add to cart
+                        </button>
+                    </div>
                 </div>
             </div>
         </>
